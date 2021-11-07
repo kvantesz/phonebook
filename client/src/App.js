87401d6f67173362a01f8453a05143d9a4f9620e
Stoica Stefan <stefann.stoica@gmail.com>
2021-11-07 15:51:14 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -6,40 +6,48 @@ import Field from "./components/Field";
 import personServices from "./services/personServices";
 
 function App() {
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [allPersons, setAllPersons] = useState([])
-  const [refreshKey, setRefreshKey] = useState(0)
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [allPersons, setAllPersons] = useState([]);
+  const [refreshKey, setRefreshKey] = useState(0);
 
   useEffect(() => {
-    personServices.getAll().then(resp => setAllPersons(resp.data))
-  },[refreshKey])
+    personServices.getAll().then((resp) => setAllPersons(resp.data));
+  }, [refreshKey]);
 
   const handleForm = (e) => {
     e.preventDefault();
 
     const newPerson = {
       name: newName,
-      number: newNumber 
-    }
+      number: newNumber,
+    };
 
-    personServices.create(newPerson)
-    
-    setNewName('')
-    setNewNumber('')
+    personServices.create(newPerson);
 
-    setRefreshKey(refreshKey + 1)
-  }
+    setNewName("");
+    setNewNumber("");
+
+    setRefreshKey(refreshKey + 1);
+  };
 
   const handleDelete = (id) => {
-    personServices.remove(id)
-    setRefreshKey(refreshKey + 1)
-  }
+    personServices.remove(id);
+    setRefreshKey(refreshKey + 1);
+  };
 
-  return <div className="App">
-    <Input newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} handleForm={handleForm} />
-    <Field allPersons={allPersons} handleDelete={handleDelete} />
-  </div>;
+  return (
+    <div className="App">
+      <Input
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        handleForm={handleForm}
+      />
+      <Field allPersons={allPersons} handleDelete={handleDelete} />
+    </div>
+  );
 }
 
 export default App;
