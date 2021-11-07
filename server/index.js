4b3fee2224a84db3db8cc8aec9803f075d2bea6b
Stoica Stefan <stefann.stoica@gmail.com>
2021-11-07 15:52:43 +0200

    test

--- server/index.js
+++ server/index.js
@@ -73,7 +73,7 @@ app.post("/api/persons", (req, resp) => {
 });
 
 //add person
-const PORT = process.env || 3003;
+const PORT = 3003;
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
